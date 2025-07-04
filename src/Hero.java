6018febe2db5baa12073624777972f8bdbc7b417
hiromotosayo <241100@std.hi-joho.ac.jp>
2025-07-04 12:53:21 +0900

    chapter13

---
+++ src/Hero.java
@@ -0,0 +1,45 @@
+public class Hero {
+    public int hp = 40;
+    private String name;
+    private Sword sword;
+
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {// 妥当性チェック
+        if (name == null) {
+            throw new IllegalArgumentException("名前がnullである。処理を中断。");
+        }
+        if (name.length() <= 1) {
+            throw new IllegalArgumentException("名前が短すぎる。処理を中断。");
+        }
+        if (name.length() >= 8) {
+            throw new IllegalArgumentException("名前が長すぎる。処理を中断。");
+        }
+        this.name = name;
+    }
+
+    public Hero(String name,int hp) {
+        this.name = name;
+        this.hp = hp;
+    }
+    void bye() {
+        System.out.println("勇者は別れを告げた");
+    }
+    private void die() {
+        System.out.println(this.name + "は死んでしまった！");
+        System.out.println("GAME OVERです。");
+    }
+    void sleep() {
+        this.hp = 100;
+        System.out.println("宿屋で回復中..." );
+    }
+    public void attack(Matango m) {
+        System.out.println(this.name + "の攻撃！");
+        System.out.println("お化けキノコ" + m.suffix + "から100ポイントの反撃を受けた");
+        this.hp -= 100;
+        if (this.hp <= 0) {
+            this.die();
+        }
+    }
+}
---
+++ src/Matango.java
@@ -0,0 +1,13 @@
+public class Matango {
+
+    int hp;
+    final int LEVEL = 10;
+    char suffix;
+
+    public Matango(char suffix) {
+        this.suffix = suffix;
+    }
+    public void run() {
+        System.out.println("お化けキノコ" + this.suffix + "は逃げ出した！");
+    }
+}
